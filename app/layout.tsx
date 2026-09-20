ebdfef9456be0e2b3e1f15a5be27914012d6badd
istanbulhighlights34-star <istanbulhighlights34@gmail.com>
2026-09-20 11:01:04 +0300

    Add Google Analytics 4 tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
+
+const GA_MEASUREMENT_ID = "G-8BXYDPK3F8";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://finalsatlas.com"),
@@ -49,6 +52,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
     </html>
   );
 }
