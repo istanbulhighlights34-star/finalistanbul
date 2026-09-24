13da3f1ea360071f2a6df08dee6ba3abd17b8514
istanbulhighlights34-star <istanbulhighlights34@gmail.com>
2026-09-25 02:25:33 +0300

    Add Arena to global navigation

--- app/_components/EventGuide.tsx
+++ app/_components/EventGuide.tsx
@@ -68,6 +68,7 @@ export default function EventGuide({
           <Link href="/cities/istanbul">Istanbul</Link>
           <Link href="/cities/madrid">Madrid</Link>
           <Link href="/cities/frankfurt">Frankfurt</Link>
+          <Link href="/arena">Arena</Link>
         </nav>
         <div className="header-actions">
           <Link className="header-blog" href="/blog">Journal</Link>
